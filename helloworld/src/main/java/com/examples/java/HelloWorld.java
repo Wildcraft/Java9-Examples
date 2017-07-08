f409cd19c3ed8f7994703b64ac71d443e4a73d04
Nagarajan Sivaraman (NAS) <Nagarajan.Sivaraman@softwareag.com>
2017-07-08 22:00:17 +0530

    adding hello world module

---
+++ helloworld/src/main/java/com/examples/java/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.examples.java;
+
+/**
+ * Created by NAS on 7/8/2017.
+ */
+public class HelloWorld {
+
+    public void print() {
+        System.out.println("This is from module - Hello World!");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Inside main");
+    }
+}
---
+++ helloworld/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+/**
+ * Created by NAS on 7/8/2017.
+ */
+module com.examples.java {
+
+}
