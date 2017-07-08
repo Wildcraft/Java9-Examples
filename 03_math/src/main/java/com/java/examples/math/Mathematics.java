4d602e712e4bfa2593ab81f32eec99855beb03f6
Nagarajan Sivaraman (NAS) <Nagarajan.Sivaraman@softwareag.com>
2017-07-09 01:25:34 +0530

    Add Math Module

---
+++ 03_math/src/main/java/com/java/examples/math/Mathematics.java
@@ -0,0 +1,7 @@
+package com.java.examples.math;
+
+/**
+ * Created by NAS on 7/9/2017.
+ */
+public class Mathematics {
+}
---
+++ 03_math/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+/**
+ * Created by NAS on 7/9/2017.
+ */
+module $MODULE_NAME$ {
+}
